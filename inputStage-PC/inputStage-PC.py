4791618aa68737bb11f70a99ee779e9cea6ac09e
Sakib Chowdhury <hi.im.sakib@gmail.com>
2018-04-24 14:22:16 -0400

    Added dependencies to plot serial data.

--- inputStage-PC/inputStage-PC.py
+++ inputStage-PC/inputStage-PC.py
@@ -2,13 +2,30 @@
 
 import serial
 import struct
+import matplotlib.pyplot as plt
+from drawnow import drawnow
+
+elecStream1 = []
+elecStream2 = []
+elecStream3 = []
+elecStream4 = []
+
+def makeFig():
+    plt.ylim(0,255)
+    plt.title("EMG Data Capture")
+    plt.grid(True)
+    plt.ylabel("Value")
+    plt.plot(elecStream1, 'ro-', label="Electrode 1")
+    plt.legend(loc="upper right")
 
 try:
     with serial.Serial('/dev/tty.usbmodem1411', 500000, timeout=1) as arduIn:
         startMessage = arduIn.readline()
         print(startMessage.decode('utf-8'))
 
-        capturing = True;
+        capturing = True
+
+        
 
         while capturing:
             try:
@@ -21,6 +38,11 @@ try:
                 print("Error: Device sent invalid data.")
                 capturing = False
             else:
-                print(dat)
+                # elecStream1.append(dat[0::4])
+                # if len(elecStream1) > 60000:
+                #     del elecStream1[0:16]
+                # print(elecStream1)
+                print(dat[0::4])
+
 except serial.SerialException:
     print("Error: Device not found.")
