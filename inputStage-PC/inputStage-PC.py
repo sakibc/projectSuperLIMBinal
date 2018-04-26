56854df4aec776487cf283ac3570eb748772541e
Sakib Chowdhury <hi.im.sakib@gmail.com>
2018-04-25 20:34:43 -0400

    Got it to graph something...

--- inputStage-PC/inputStage-PC.py
+++ inputStage-PC/inputStage-PC.py
@@ -1,22 +1,43 @@
+# ==============================================================================
 # Copyright 2018 Sakib Chowdhury and Claudia Lutfallah
+# ==============================================================================
+# Project SuperLIMBinal inputStage-PC.py
+# This script monitors the serial port and captures the data in chunks of 64
+# bytes as the Arduino sends it at its maximum transfer rate. It separates the
+# data out of the chunk into arrays for each electrode and displays it on the
+# screen.
+# ==============================================================================
+
+# Imports ======================================================================
 
 import serial
 import struct
+
 import matplotlib.pyplot as plt
-from drawnow import drawnow
+import numpy as np
 
-elecStream1 = []
-elecStream2 = []
-elecStream3 = []
-elecStream4 = []
+# Variables ====================================================================
 
-def makeFig():
-    plt.ylim(0,255)
-    plt.title("EMG Data Capture")
-    plt.grid(True)
-    plt.ylabel("Value")
-    plt.plot(elecStream1, 'ro-', label="Electrode 1")
-    plt.legend(loc="upper right")
+baudRate = 500000 # any slower and the Arduino can't send the data fast enough
+
+# sampling rate overall is 16MHz/(32prescale*13cycles) ~= 38462
+# for each channel, at 4 channels is ~9615 Hz
+
+samplingRate = 9616 # make it an even multiple of 16 to ease data capture
+timePeriod = 6      # for now let's just graph 6 seconds worth of data
+
+elecStream1 = np.zeros(samplingRate*timePeriod)
+elecStream2 = np.zeros(samplingRate*timePeriod)
+elecStream3 = np.zeros(samplingRate*timePeriod)
+elecStream4 = np.zeros(samplingRate*timePeriod)
+
+t = 0
+timex = np.arange(0,samplingRate*timePeriod)
+
+# Logic ========================================================================
+
+def EMGgraph():
+    plt.plot(timex, elecStream1)
 
 try:
     with serial.Serial('/dev/tty.usbmodem1411', 500000, timeout=1) as arduIn:
@@ -24,8 +45,6 @@ try:
         print(startMessage.decode('utf-8'))
 
         capturing = True
-
-        
 
         while capturing:
             try:
@@ -38,11 +57,12 @@ try:
                 print("Error: Device sent invalid data.")
                 capturing = False
             else:
-                # elecStream1.append(dat[0::4])
-                # if len(elecStream1) > 60000:
-                #     del elecStream1[0:16]
-                # print(elecStream1)
-                print(dat[0::4])
+                if t < 9616*6:
+                    elecStream1[t:(t+16)] = dat[0::4]
+                    t += 16
+                else:
+                    elecStream1 = np.append(elecStream1[16:],dat[0::4])
+                    timex += 16
 
 except serial.SerialException:
     print("Error: Device not found.")
