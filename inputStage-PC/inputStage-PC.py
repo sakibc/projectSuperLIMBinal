49a433a2b547a950da01ddba29d0a2ecd399402f
Sakib Chowdhury <hi.im.sakib@gmail.com>
2018-04-27 22:03:29 -0400

    It works, it graphs!

--- inputStage-PC/inputStage-PC.py
+++ inputStage-PC/inputStage-PC.py
@@ -9,10 +9,12 @@
 import serial, struct, csv
 import time, datetime
 import sys
+import queue
 
 import matplotlib
-matplotlib.use('Qt5Agg') # don't need python as a framework
+matplotlib.use('tkagg')
 
+import multiprocessing as mp
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
@@ -24,13 +26,14 @@ baudRate = 500000 # any slower and the Arduino can't send the data fast enough
 port = '/dev/tty.usbmodem1411'  # port number for right-most USB port on a
 # Macbook Air running macOS. For Windows 10 you'll have to change this to
 # "COMX", determined from Device Manager.
+#TODO: Autodetect Arduino port
 
 # sampling rate overall is 16MHz/(32prescale*13cycles) ~= 38462
 # for each channel, at 4 channels is ~9615 Hz
+# we sample 601 times per second, which is 10 samples per frame at 60 fps.
 
 samplingRate = 9616 # make it an even multiple of 16 to ease data capture
-captureTime = 6 # for now let's just save 6 seconds worth of data
-sampleNo = 0
+captureTime = 20
 
 # array to save data to import into MATLAB to determine how to process it.
 # once a processing algorithm is determined it the algorithm will be rewritten
@@ -38,74 +41,136 @@ sampleNo = 0
 # the data once we capture it
 elecData = np.zeros((4,samplingRate*captureTime))
 
-# t = np.arange(0,samplingRate*timePeriod)    #graph boundaries
-
 # Logic
 
-# fig = plt.figure(1)
-# ax1 = fig.add_subplot(411)
-# line1, = ax1.plot(t, elecStream1)
+drawTime = 10 # only show the last 6 seconds of data on the screen
 
-# ax1.set_xlabel("Sample number")
-# ax1.set_ylabel("Activation level")
-# # ax1.set_xlim(t[0],t[-1])
-# ax1.set_ylim(0,255)
+plt.style.use('seaborn-darkgrid')
 
-# ax1.grid(True)
+fig, ax = plt.subplots(4,1,sharex=True)
+line = [ax[i].plot([],[])[0] for i in range(4)]
+xdata = []
+ydata = [[],[],[],[]]
 
-# fig.canvas.draw()
+fig.suptitle("EMG Sensor Data")
+fig.text(0.5,0,"Time", ha='center')
+fig.text(0.04,0.5,"Normalized Activation Level", va='center', rotation='vertical')
 
-try:
-    with serial.Serial(port, baudRate, timeout=1, rtscts=True, dsrdtr=None) as arduIn:
-        startMessage = arduIn.readline()
-        print(startMessage.decode('utf-8'))
+def data_gen(t=0):
+    while True:
+        t += 1/30
+        try:
+            dat = q.get(block=True, timeout=0.5)
+            yield t, dat
+        except queue.Empty:
+            break
 
-        print("Capturing data...")
-        capturing = True
-        startTime = time.time() # for now let's capture a set amount of data
+def init():
+    del xdata[:]
 
-        while capturing:
-            try:
-                rawdat = arduIn.read(64)
-                dat = struct.unpack('64B',rawdat)
-            except serial.SerialException:
-                print("Error: Connection lost.")
-                capturing = False
-            except struct.error:
-                print("Error: Device sent invalid data.")
-                capturing = False
-            else:
-                if sampleNo >= samplingRate*captureTime:
-                    capturing = False   # we're done capturing
-                    break
+    for i in range(4):
+        ax[i].set_ylim(0,1)
+        ax[i].set_xlim(0,drawTime)
+    
+        del ydata[i][:]
 
-                elecData[0][sampleNo:(sampleNo+16)] = dat[0::4]
-                elecData[1][sampleNo:(sampleNo+16)] = dat[1::4]
-                elecData[2][sampleNo:(sampleNo+16)] = dat[2::4]
-                elecData[3][sampleNo:(sampleNo+16)] = dat[3::4]
+        line[i].set_data(xdata, ydata[i])
 
-                sampleNo += 16
+    return line
 
-                # updatePlot()
-                # if sampleNo < 9616*6:  #for plotting
-                #     elecStream1[sampleNo:(sampleNo+16)] = dat[0::4]
-                #     sampleNo += 16
-                # else:
-                #     elecStream1 = np.append(elecStream1[16:],dat[0::4])
-                #     t += 16
+def run(data):
+      # update the data
+    t, elec = data
+    xdata.append(t)
+    for i in range(4):
+        ydata[i].append(elec[i]/255)
 
-        print("\nData capture complete.")
-        
-        csvfile = datetime.datetime.fromtimestamp(startTime).strftime("%Y%m%d-%H%M%S") + ".csv"
-        csvfile = "../inputStage-analysis/capturedData/" + csvfile
+        line[i].set_data(xdata, ydata[i])
+        xmin,xmax = ax[i].get_xlim()
 
-        np.savetxt(csvfile, elecData, fmt="%d", delimiter=",")
+        if t >= xmax:
+            ax[i].set_xlim(xmin+1,xmax+1)
+            ax[i].figure.canvas.draw()
 
-        print("Data saved to", csvfile)
+    return line
 
-except serial.SerialException:
-    print("Error: Device not found.")
-except KeyboardInterrupt:
-    print("\nProgram execution interrupted.")
+def capture(q):
+    sampleNo = 0
+    queueTimer = 0  # every 10 sets of samples, push some data to the queue for the graph to plot.
 
-sys.exit()
+    try:
+        with serial.Serial(port, baudRate, timeout=1, dsrdtr=True) as arduIn:
+            print("Opening port...")
+
+            startMessage = arduIn.readline()
+            q.put(startMessage.decode('utf-8'))
+
+            print("Initializing graph...")
+            capturing = True
+            startTime = time.time() # for now let's capture a set amount of data
+
+            while capturing:
+                try:
+                    rawdat = arduIn.read(64)
+                    dat = struct.unpack('64B',rawdat)
+
+                except serial.SerialException:
+                    print("Error: Connection lost.")
+                    print("\nSaving stored data...")
+                    capturing = False
+
+                except struct.error:
+                    print("Error: Device sent invalid data.")
+                    print("\nSaving stored data...")
+                    capturing = False
+
+                else:
+                    elecData[0][sampleNo:(sampleNo+16)] = dat[0::4]
+                    elecData[1][sampleNo:(sampleNo+16)] = dat[1::4]
+                    elecData[2][sampleNo:(sampleNo+16)] = dat[2::4]
+                    elecData[3][sampleNo:(sampleNo+16)] = dat[3::4]
+
+                    sampleNo += 16
+
+                    queueTimer += 1
+
+                    if queueTimer % 20 == 0:
+                        queueTimer = 0
+                        q.put([dat[0],dat[1],dat[2],dat[3]])    # we don't need to show all the data on the graph...
+                        
+
+                    if sampleNo >= samplingRate*captureTime:
+                        capturing = False   # we're done capturing
+                        print("Data capture complete.")
+
+
+            csvfile = datetime.datetime.fromtimestamp(startTime).strftime("%Y%m%d-%H%M%S") + ".csv"
+            csvfile = "../inputStage-analysis/capturedData/" + csvfile
+
+            np.savetxt(csvfile, elecData, fmt="%d", delimiter=",")
+
+            print("Data saved to", csvfile)
+            print("Close live plot to continue.")
+
+    except serial.SerialException:
+        print("Error: Device not found.")
+    except KeyboardInterrupt:
+        print("\nProgram execution interrupted.")
+
+    #TODO: Handle exceptions from child process better...
+
+if __name__ == "__main__":
+    q = mp.Queue()
+    p = mp.Process(target=capture, args=(q,))   #create separate process to handle
+    print("Starting capture process...")        #data capture so that it's not blocked
+    p.start()                                   #by the live graph.
+    print(q.get())
+
+    ani = animation.FuncAnimation(
+        fig, run, data_gen, blit=True, interval=10, repeat=False, init_func=init)
+
+    mng = plt.get_current_fig_manager() # maximize window, because it doesn't like
+    mng.resize(*mng.window.maxsize())   # being maximized after the fact...
+    plt.show()
+
+    p.join()
