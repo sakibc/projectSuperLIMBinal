af3a2e90e12bd0801e9f261a1cac1afef726298b
Sakib Chowdhury <hi.im.sakib@gmail.com>
2018-05-19 00:20:03 -0400

    successful proof of concept and test of prediction  on low-quality data

--- inputStage-analysis/analysisTest.m
+++ inputStage-analysis/analysisTest.m
@@ -1,8 +1,11 @@
+% training the matrix for 2 DOFs: open/close and pronate/supinate
+
 close all;
 clc;
 clear all;
 
 load capturedData/set2;
+
 elecData = elecData.' - 128; % centre amplitude on 0
 Fs = 4808;
 
@@ -13,20 +16,66 @@ for i = 1:6 % comb filter? get rid of that mains hum...
     elecData = filtfilt(b,a,elecData);
 end
 
-d = designfilt('bandpassfir',...
-    'FilterOrder',20,...
-    'CutoffFrequency1',10,...
-    'CutoffFrequency2',300,...
-    'SampleRate',Fs);
-% fvtool(d,'Fs',Fs);
-elecData = abs(filtfilt(d,elecData));
+elecData = elecData.^2;
+windowLen = round(0.5*Fs);
+window = ones(1,windowLen)/windowLen;
+smoothedData = filter(window,1,elecData);
 
-N = 8; % number of channels
-K = 4; % number of synergies
+timeStart = ((0:9:45) + 8)*Fs;
+timeEnd = timeStart + 3*Fs;
+
+L = 8; % number of channels
+N = 4; % number of synergies
 Ob = length(elecData); % number of observations
 
-window = rectwin(round(0.2*Fs));
+% baseline
+relaxedData = smoothedData(timeStart(1):timeEnd(1),:);
+baselines = mean(relaxedData);
 
-dat = conv2(elecData,window,'same');
+for i = 1:8
+    smoothedData(:,i) = smoothedData(:,i) - baselines(i);
+    smoothedData(:,i) = smoothedData(:,i)/max(smoothedData(:,i));
+end
 
-% signalAnalyzer(data,'SampleRate',Fs);
+% first DOF
+openData = smoothedData(timeStart(2):timeEnd(2),:).';
+closeData = smoothedData(timeStart(3):timeEnd(3),:).';
+
+% second DOF
+proData = smoothedData(timeStart(4):timeEnd(4),:).';
+soupData = smoothedData(timeStart(5):timeEnd(5),:).';
+
+[W1,H1] = nnmf(openData,1,'algorithm','mult');  % assuming all but one channels of H are inactive
+[W2,H2] = nnmf(closeData,1,'algorithm','mult'); % this totally works, okay?
+[W3,H3] = nnmf(proData,1,'algorithm','mult');
+[W4,H4] = nnmf(soupData,1,'algorithm','mult');
+
+H = [H1;H2;H3;H4];
+maxes = max(H.');
+W = [W1*maxes(1),W2*maxes(2),W3*maxes(3),W4*maxes(4)];  % is this it? did I win?
+
+load capturedData/set3; % cross validation set, loaded in as elecData
+
+elecData = elecData.' - 128; % centre amplitude on 0
+for i = 1:6 % comb filter? get rid of that mains hum...
+    wo = i*60/(Fs/2); bw = wo/35; % notch filter to remove 60Hz noise
+    [b,a] = iirnotch(wo,bw);
+
+    elecData = filtfilt(b,a,elecData);
+end
+
+elecData = elecData.^2;
+smoothedData = filter(window,1,elecData);
+
+% baseline
+relaxedData = smoothedData(timeStart(1):timeEnd(1),:);
+baselines = mean(relaxedData);
+
+for i = 1:8
+    smoothedData(:,i) = smoothedData(:,i) - baselines(i);
+    smoothedData(:,i) = smoothedData(:,i)/max(smoothedData(:,i));
+end
+
+F = smoothedData/W.';
+F(F<0) = 0;
+
