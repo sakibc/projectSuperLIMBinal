f884670ac58d85e1786a7de0c252b64f4dc41160
Sakib Chowdhury <hi.im.sakib@gmail.com>
2018-07-14 20:17:58 -0400

    remove brain from pineapple

--- inputStage-PC/frontend-dev/src/main.js
+++ inputStage-PC/frontend-dev/src/main.js
@@ -7,7 +7,13 @@ import router from './router'
 import socketio from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
 
-export const SocketInstance = socketio('http://localhost:5000')
+export let SocketInstance = global.SocketInstance
+
+if (process.env.NODE_ENV === 'development') {
+  SocketInstance = socketio('http://localhost:5000')
+} else {
+  SocketInstance = socketio()
+}
 
 Vue.config.productionTip = false
 Vue.use(VueSocketIO, SocketInstance)
